7b525abba09b1931a334e38386b0164223ff682e
John Fisher <jgf5013@gmail.com>
2020-04-09 23:13:51 -0400

    Two cards for two projects - mobile friendly

---
+++ build/precache-manifest.8d4ffb6b848558009af0465a7d16fa08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62001714344d42b21fa0e1102a6eb1c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7776211ba6c27197e98a",
+    "url": "/static/css/main.a9bdc318.chunk.css"
+  },
+  {
+    "revision": "e07c0f8118354d76fb33",
+    "url": "/static/js/2.ad768492.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ad768492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7776211ba6c27197e98a",
+    "url": "/static/js/main.814e2909.chunk.js"
+  },
+  {
+    "revision": "937dcc9f6e96ff7afd20",
+    "url": "/static/js/runtime-main.5ea17a3a.js"
+  }
+]);
